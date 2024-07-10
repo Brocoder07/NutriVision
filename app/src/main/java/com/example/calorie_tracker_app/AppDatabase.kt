ecb0e2fa87a58b2178eceda99e148f4f802fa670
Akshay Manjunath <157276831+Brocoder07@users.noreply.github.com>
2024-07-10 22:14:20 +0530

    Remove all files

--- app/src/main/java/com/example/calorie_tracker_app/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.calorie_tracker_app
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [ImageEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun imageDao(): ImageDao
-}
--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.android.library") version "8.0.2" apply false
-}
-tasks.register<Delete>("clean") {
-    delete(layout.buildDirectory)
-}
